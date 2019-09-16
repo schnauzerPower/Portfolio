b5a48c3ae016ea3e5263a350c7a39eb99600c154
Will Albertsen <talktowillalbertsen@gmail.com>
2019-09-16 19:04:45 -0400

    Add files and update app.js

--- app.js
+++ app.js
@@ -1,11 +1,46 @@
-const express = require('express');
+const express= require('express');
 const app = express();
+const data = require('./data.json');
 
+
+//Use Pug
 app.set('view engine', 'pug');
 
+//Serve static files
+app.use('/static', express.static('public'));
+
+//Route to homepage
 app.get('/', (req, res) => {
-    res.send("Fuck you!");
-})
+    res.render('index', {data});
+});
 
+//Route to about page
+app.get('/about', (req, res) => {
+    const twitter = 'https://twitter.com/whalbee'
+    const linkedIn = 'https://www.linkedin.com/in/will-albertsen-a976a7191/'
+    const gitHub = 'https://github.com/schnauzerPower'
+    const templateData = {gitHub, linkedIn, twitter};
+    res.render('about', templateData);
+});
 
+//Route to all of the project pages
+app.get('/project/:id', (req, res) => {
+    const {id} = req.params
+    res.render('project', {data, id})
+});
+
+//Error middleware
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    res.locals.error = err;
+    res.status(err.status);
+    res.render('error');
+});
+
+//Display app on localhost
 app.listen(3000);
